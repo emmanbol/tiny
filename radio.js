c9fa7ee94ec6569e4d374404a4fd1b75714828cd
emmanbol <emmanbol@gmail.com>
2022-05-10 13:27:00 +0100

    test5

--- radio.js
+++ radio.js
@@ -9,15 +9,22 @@ this.output = fish(15, 20)
 
 class NewKlass{
 
-    ayy = () => {
+    constructor(a, b, c){
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
 
+    ayy = (d) => {
+        return this.a * this.b * d;
     }
 
     bay = (a, b, c) => {
-        console.log(`the three input is: ${a}, ${b}, ${c}`)
+        console.log(`the three input is: ${this.a}, ${this.b}, ${this.c}`)
     }
 }
 
-let collect1 = new NewKlass()//
+let kk = new NewKlass(2, 4, 8)//
 
-collect1.bay()
+kk.bay()
+console.log(`output is: ${kk.ayy(5)}`)
