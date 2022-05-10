4ec28686233685caa2f5d6aebc796df5eb4b2997
emmanbol <emmanbol@gmail.com>
2022-05-10 13:18:16 +0100

    test3

--- radio.js
+++ radio.js
@@ -2,6 +2,22 @@ console.log('my fish is 10cm long')
 
 const fish = (first, second) => {
     console.log(`The first is: ${first} and the second is: ${second}`)
+    return first*second;
 }
 
-fish(15, 20)
+this.output = fish(15, 20)
+
+class NewKlass{
+
+    ayy = () => {
+
+    }
+
+    bay = (a, b, c) => {
+        console.log(`the three input is: ${a}, ${b}, ${c}`)
+    }
+}
+
+let collect1 = new NewKlass()
+
+collect1.bay()
