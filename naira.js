85ba6bee99e0088590408e7edb0b1abcd92983e4
emmanbol <emmanbol@gmail.com>
2022-05-10 14:30:21 +0100

    test518

---
+++ naira.js
@@ -0,0 +1,24 @@
+class Musa{
+
+    kk = (a, b, c) => {
+        console.log(a*b*c)
+    }
+
+    abc(){
+        console.log(2*3*5)
+    }
+
+
+}
+
+function mss(a, x){
+    console.log(a*x)
+}
+
+let mm = new Musa()
+
+mm.kk(1,2,3)
+
+mm.abc()
+
+mss(2, 45)
